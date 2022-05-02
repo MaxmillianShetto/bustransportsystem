a8a0f2e9999e78f403574394316c733b32c596d0
MaxmillianShetto <90018258+Maxmillian-Shetto@users.noreply.github.com>
2022-05-02 09:27:36 +0200

    Middleware to validate if user is a driver, and is authorized to perform an action.

--- backend/Middleware.js
+++ backend/Middleware.js
@@ -2,31 +2,37 @@ const e = require('express');
 const { User } = require('./models');
 const jwt = require('jsonwebtoken');
 
+// This function checks if requests are comming from
+// a real user 
 exports.checkPassenger = async (req, res, next) => {
     try{
-        // validating user
         const token = req.headers["auth-token"];
-        // console.log(token);
         const decodedToken = jwt.decode(token, 'skdnvkdsjnvsdkjn');
         const user = await User.findById(decodedToken._id);
-        if (decodedToken._id === req.body.passenger ||
-            user)
-        {
+        if (decodedToken._id === req.body.passenger || user.role === "passenger"){
             next();
         }
-        else{
-            return res.json({
-                status: 400,
-                message: "Unauthorized attempt",
-            });
+        else {
+            return res.json({status: 400, message: "You have to be a passenger to perform this operation"});
         }
+    } catch(err){
+        return res.json({status: 401, message: "Something went wrong, please try again later"}); 
     }
-    catch(err)
-    {
-        return res.json({
-            status: 401,
-            message: "Something went wrong, please try again later",
-        });
+}
+
+exports.checkDriver = async (req, res, next) => {
+    try{
+        const token = req.headers["auth-token"];
+        const decodedToken = jwt.decode(token, 'skdnvkdsjnvsdkjn');
+        const user = await User.findById(decodedToken._id);
+        if (user.role === "Driver"){
+            next();
+        }
+        else {
+            return res.json({status: 400, message: "You have to be a driver to perform this operation"});
+        }
+    } catch(err){
+        return res.json({status: 401, message: "Something went wrong, please try again later"}); 
     }
 
 }
