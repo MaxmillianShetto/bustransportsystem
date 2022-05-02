d21bbf0721eb45c028fd43e6f0392de590157496
MaxmillianShetto <90018258+Maxmillian-Shetto@users.noreply.github.com>
2022-05-02 14:40:23 +0200

    Changed the color theme a bit

--- frontend/src/pages/RoutesPage/index.jsx
+++ frontend/src/pages/RoutesPage/index.jsx
@@ -1,89 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './RoutesPage.css';
-import axios from 'axios';
-import Button from '../../components/Button';
-import { trimmed } from '../../helpers';
-import InputTextField from '../../components/InputText';
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { toast } from 'react-toastify';
+import "./RoutesPage.css";
+import axios from "axios";
+import Button from "../../components/Button";
+import { trimmed } from "../../helpers";
+import InputTextField from "../../components/InputText";
 
 const RoutesPage = (props) => {
-    const [route, setRoute] = useState({
-        routeName: '',
+  const [route, setRoute] = useState({
+    routeName: "",
+  });
+  const [error, setError] = useState("");
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    if (error) {
+      setError("");
+    }
+
+    setRoute({
+      ...route,
+      [name]: value,
     });
-    const [error, setError] = useState('');
-    const handleChange = ({ target }) => {
-        const { name, value } = target;
-    
-        if (error) {
-          setError('');
-        }
-    
-        setRoute({
-          ...route,
-          [name]: value
-        });
-    };
-    useEffect(() => {
-        // remove the current state from local storage
-        // so that when a person logs in they dont encounter
-        // the previous state which wasnt cleared
-        localStorage.removeItem('state');
-    }, []);
+  };
+  useEffect(() => {
+    // remove the current state from local storage
+    // so that when a person logs in they dont encounter
+    // the previous state which wasnt cleared
+    localStorage.removeItem("state");
+  }, []);
 
-    const submit = () => {
-        const { routeName } = route;
-    
-        const routeInfo = {
-          route: trimmed(routeName)
-        }
-    
-        if (!routeInfo.route) {
-          setError('Route is required');
-          return;
-        }
-        const access_token = localStorage.getItem('token');
-        axios.post('/api/WeGo/route', routeInfo, 
-        {headers: {'auth-token': `${access_token}`}})             //!   Needs to be changed
-          .then(res => {
-            console.log(res.data)
-            alert("hi")
-            window.location.href = "/admin";
-          })
-          .catch((err) => {
-            setError('Incorrect email or password.');
-            console.log(err);
-        });
-    };
-    
-    return(
-        <div className="Page">
-            <div className="Form">
-                <div className="FormTitle">Add Route</div>
-        
-                <InputTextField
-                    required
-                    type="text"
-                    name="routeName"
-                    value={route.routeName}
-                    placeholder="Route"
-                    onChange={handleChange}
-                />
-        
-                {error && (
-                    <div className="Error">
-                    {error}
-                    </div>
-                )}
-        
-                <Button
-                    label="ADD ROUTE"
-                    onClick={submit}
-                />
-    
-            </div>
-        </div>
-    )
+  const submit = () => {
+    const { routeName } = route;
 
-}
-export default connect(  )(withRouter(RoutesPage));
+    const routeInfo = {
+      route: trimmed(routeName),
+    };
+
+    if (!routeInfo.route) {
+      setError("Route is required");
+      return;
+    }
+    const access_token = localStorage.getItem("token");
+    axios
+      .post("/api/WeGo/route", routeInfo, {
+        headers: { "auth-token": `${access_token}` },
+      }) //!   Needs to be changed
+      .then((res) => {
+        console.log(res.data);
+        toast.success("Hiiiiiiiii");
+        // alert("hi")
+        // window.location.href = "/admin";
+      })
+      .catch((err) => {
+        setError("Incorrect email or password.");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="Page">
+      <div className="Form">
+        <div className="FormTitle">Add Route</div>
+
+        <InputTextField
+          required
+          type="text"
+          name="routeName"
+          value={route.routeName}
+          placeholder="Route"
+          onChange={handleChange}
+        />
+
+        {error && <div className="Error">{error}</div>}
+
+        <Button label="ADD ROUTE" onClick={submit} />
+      </div>
+    </div>
+  );
+};
+export default connect()(withRouter(RoutesPage));
