4aa4daa9600b1f47bcb9451b176ff6ed21f8e404
MaxmillianShetto <90018258+Maxmillian-Shetto@users.noreply.github.com>
2022-05-02 19:07:41 +0200

    display list of passengers

--- frontend/src/components/ViewPassengersPage/index.jsx
+++ frontend/src/components/ViewPassengersPage/index.jsx
@@ -5,10 +5,11 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import "./ViewPassengersPage.css";
 import Navbar from "../Navbar";
-import { dateTime } from '../../helpers/dateConversion';
+import { dateTime } from "../../helpers/dateConversion";
+import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
@@ -17,6 +18,26 @@ import Paper from "@mui/material/Paper";
 const ViewPassengersPage = (props) => {
   const [passengers, setPassengers] = useState([]);
   const [error, setError] = useState("");
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#54A0EE",
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
 
   useEffect(() => {
     // remove the current state from local storage
@@ -29,7 +50,7 @@ const ViewPassengersPage = (props) => {
       })
       .then((res) => {
         console.log(res.data);
-        // setPassengers(res.data);
+        setPassengers(res.data);
       })
       .catch((err) => {
         setError("Process failed.");
@@ -45,20 +66,46 @@ const ViewPassengersPage = (props) => {
       <Navbar />
       <div className="Form">
         <div className="FormTitle">List of passengers</div>
-        {passengers !== null
-          ? passengers
-              .slice(0)
-              .reverse()
-              .map((passenger, index) => (
-                <div className="passenger" key={index}>
-                  <div className="Username">{passenger.passenger.firstname + " " +
-                  passenger.passenger.lastname}</div>
-                  <div className="Time">{dateTime(passenger.pickupTime)}</div>
-                  <div className="Departure">{passenger.departureLocation}</div>
-                  <div className="Dest">{passenger.destinationLocation}</div>
-                </div>
-              ))
-          : ""}
+        {passengers !== null ? (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Username</StyledTableCell>
+                  <StyledTableCell align="right">Time</StyledTableCell>
+                  <StyledTableCell align="right">Pickup</StyledTableCell>
+                  <StyledTableCell align="right">Destination</StyledTableCell>
+                  <StyledTableCell align="right">Seats</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {passengers.map((passenger, index) => (
+                  <StyledTableRow key={index}>
+                  <StyledTableCell component="th" scope="row">
+                    {passenger.passenger.firstname}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{passenger.pickupTime}</StyledTableCell>
+                  <StyledTableCell align="right">{passenger.departureLocation}</StyledTableCell>
+                  <StyledTableCell align="right">{passenger.destinationLocation}</StyledTableCell>
+                  <StyledTableCell align="right">{passenger.numberOfSits}</StyledTableCell>
+                </StyledTableRow>
+                // <div className="passenger" key={index}>
+                //   <div className="Username">
+                //     {this.state.passenger.passenger.firstname +
+                //       " " +
+                //       this.state.passenger.passenger.lastname}
+                //   </div>
+                //   <div className="Time">{dateTime(this.state.passenger.pickupTime)}</div>
+                //   <div className="Departure">{this.state.passenger.departureLocation}</div>
+                //   <div className="Dest">{this.state.passenger.destinationLocation}</div>
+                // </div>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          ""
+        )}
         {error && <div className="Error">{error}</div>}
       </div>
     </div>
